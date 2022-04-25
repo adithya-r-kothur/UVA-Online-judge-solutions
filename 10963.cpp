72eaeb11e9548f5cec17ad87f1b31c566b92fad7
Adithya.r.Kothur <90601205+adithya-r-kothur@users.noreply.github.com>
2022-04-25 18:53:10 +0530

    Add files via upload

--- 10963.cpp
+++ 10963.cpp
@@ -1,11 +1,31 @@
-#include<bits/stdc++.h>
-
-
+#include <cstdio>
 using namespace std;
 
-int main()
-{
-    int t;
-    
-    return 0;
+int main() {
+	int tc, n, up, down;
+
+	scanf("%d", &tc);
+	while (tc--) {
+		scanf("%d", &n);
+
+		bool yes = true;
+		scanf("%d %d", &up, &down);
+		int d = up - down;
+
+		for (int i = 0; i < n - 1; i++) {
+			scanf("%d %d", &up, &down);
+			if (d != up - down)
+				yes = false;
+		}
+
+		if (yes)
+			printf("yes\n");
+		else
+			printf("no\n");
+
+		if (tc)
+			printf("\n");
+	}
+
+	return 0;
 }
