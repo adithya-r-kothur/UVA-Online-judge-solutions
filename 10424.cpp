3190863eb4f41143b9da4d88f188a9c269335f90
adithya <adithya.r.kothur@gmail.com>
2022-05-04 00:58:46 +0530

    initial commit

---
+++ 10424.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int  main()
+{
+   char a[100];
+   char b[100];
+
+   while(scanf("%c" "%c" ,&a,&b) !=EOF) 
+   {
+       
+   }
+    return 0;
+}
